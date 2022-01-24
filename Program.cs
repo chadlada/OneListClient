396b2f7e6c2b2cb23ef110e98ef0ae2274b95ff3
Chad <chadlada@gmail.com>
2022-01-24 18:13:38 -0500

    Response as string with Console Writeline

--- Program.cs
+++ Program.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace OneListClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var client = new HttpClient();
+            var responseBodyAsString = await client.GetStringAsync("https://one-list-api.herokuapp.com/items/1?access_token=sdg-handbook");
+            Console.WriteLine(responseBodyAsString);
         }
     }
 }
